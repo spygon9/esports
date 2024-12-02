56ac8c11e9d45300ba06247664152f4cc69d5df7
Spygon9 <david_acav@hotmail.com>
2024-12-02 02:30:56 -0600

    Creación de seeder

---
+++ esports.Api/Migrations/20241202080943_Relaciones.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace esports.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Relaciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ esports.Api/Seeder.cs
@@ -0,0 +1,49 @@
+﻿
+using esports.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace esports.Api
+{
+    public class Seeder
+    {
+        private readonly DataContext dataContext;
+
+        public Seeder(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        public async Task SeedAsync()
+        {
+            await dataContext.Database.EnsureCreatedAsync();
+            await CheckTeamsAsync();
+            await CheckUsersAsync();
+        }
+
+        private async Task CheckTeamsAsync()
+        {
+            if (!dataContext.Teams.Any())
+            {
+                dataContext.Teams.Add(new Team { TeamName = "T1" });
+                dataContext.Teams.Add(new Team { TeamName = "GenG" });
+                dataContext.Teams.Add(new Team { TeamName = "FlyQuest" });
+                dataContext.Teams.Add(new Team { TeamName = "Rainbow7" });
+            }
+        }
+
+        private async Task CheckUsersAsync()
+        {
+            if(!dataContext.Users.Any())
+            {
+                var team = await dataContext.Teams.FirstOrDefaultAsync(x => x.TeamName == "T1");
+                if (team != null)
+                {
+                    dataContext.Users.Add(new User { Username = "Faker", Team = team });
+                    dataContext.Users.Add(new User { Username = "Oner", Team = team });
+                    dataContext.Users.Add(new User { Username = "Zeus", Team = team });
+                    dataContext.Users.Add(new User { Username = "Gumayusi", Team = team });
+                    dataContext.Users.Add(new User { Username = "Keria", Team = team });
+                }
+            }
+        }
+    }
+}
